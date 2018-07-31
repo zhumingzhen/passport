91a83c759f9b0e030fb88e51b1853f9e9a3a139f
itdream <itdream6@163.com>
2018-07-31 19:12:01 +0800

    wechat

---
+++ app/Http/Controllers/WechatTestControoler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WechatTestControoler extends Controller
+{
+    //demo
+    function test(){
+        phpinfo();
+    }
+}

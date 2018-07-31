6e0da870553af9b20f93f054a3d5467996bd9631
vagrant <vagrant@homestead>
2018-07-31 08:12:45 +0000

    first commit

---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WechatController extends Controller
+{
+  protected $app;
+
+  public function __construct()
+  {
+      $this->app = app('wechat.official_account');
+  }
+
+  /**
+     * 处理微信的请求消息
+     *
+     * @return string
+     */
+    public function serve()
+    {
+        $this->app->server->push(function($message){
+            return "欢迎关注 itdream6@163.com！";
+        });
+        return $this->app->server->serve();
+    }
+}

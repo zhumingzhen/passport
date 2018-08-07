1d3e4d9480018f25518546ffe34e37624881a80b
itdream <itdream53@gmail.com>
2018-08-07 10:23:33 +0800

    redis

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Proxy\TokenProxy;
 use App\User;
 use App\Wechat;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Redis;
 
 class LoginController extends Controller
 {
--- app/Wechat.php
+++ app/Wechat.php
@@ -8,5 +8,5 @@ class Wechat extends Model
 {
     protected $table = 'wechat';
 
-    protected $fillable = ['user_id','openid','nickname','sex','language','city','province','country','avatar'];
+    protected $fillable = ['user_id','unionld','openid','nickname','sex','language','city','province','country','avatar'];
 }

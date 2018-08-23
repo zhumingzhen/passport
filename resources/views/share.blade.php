b624802b6caca2efb3b4742a460abcc3c1159a7e
itdream <itdream53@gmail.com>
2018-08-23 18:35:26 +0800

    share 请求地址修改

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -1,4 +1,4 @@
 <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
-    wx.config(<?php echo $app->jssdk->buildConfig(array('onMenuShareQQ', 'onMenuShareWeibo'), true) ?>);
+    wx.config(<?php echo $app->jssdk->setUrl('http://passport.it1.me')->buildConfig(array('onMenuShareQQ', 'onMenuShareWeibo'), true) ?>);
 </script>
